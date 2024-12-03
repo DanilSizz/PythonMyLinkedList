39cfb59e7318ec909674d05683e540b9383dea9c
DanilSizz <theghost.teenager@gmail.com>
2024-12-03 15:41:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-class Slivik:
+class Node:
+
+    def __init__(self, value):
+
+        self.value = value
+        self.next = None
+
+class LinkedList:
 
     def __init__(self):
 
@@ -6,17 +13,10 @@ class Slivik:
 
         self.first_element = None
         self.last_element = None
-
-    class Node:
-
-        def __init__(self, value):
-
-            self.value = value
-            self.next = None
     
     def append(self, value):
 
-        new_node = self.Node(value)
+        new_node = Node(value)
 
         if self.first_element is None:
             self.first_element = new_node
@@ -28,31 +28,45 @@ class Slivik:
         
         self.size += 1
     
+    def set_value(self, index, value):
+
+        if index < 0 or index >= self.size:
+            raise IndexError("Index out of range")
+
+        current_elem = self.first_element
+        for _ in range(0, index):
+            current_elem = current_elem.next
+
     def get_value(self, index):
 
         if index < 0 or index >= self.size:
             raise IndexError("Index out of range")
 
-        
         current_elem = self.first_element
         for _ in range(0, index):
             current_elem = current_elem.next
 
         return current_elem.value
 
+    def remove_value(self, index):
+
+        removable = self.get_value(index)
+        
+        for _ in range(self.size - 1):
+            if _ == index:
+                pass
+
+
     def sort(self, myarray):
 
         current = self.first_element
-        lis1 = Slivik()
-        lis2 = Slivik()
-
+        lis1 = LinkedList()
+        lis2 = LinkedList()
 
         for _ in range(self.size):
             if current.next != None:
                 if current > current.next:
                     pass
-
-
 
     def __str__(self):
 
@@ -67,7 +81,7 @@ class Slivik:
     def __len__(self):
         return self.size
 
-myarray = Slivik()
+myarray = LinkedList()
 
 myarray.append('хелоу воролд')
 
